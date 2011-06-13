e601c0d2601d5e70f29977173fe3d77e7cd70ffa
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2011-06-13 23:49:31 +0900

    use capybara

--- capybara-learning/spec/requests/articles_spec.rb
+++ capybara-learning/spec/requests/articles_spec.rb
@@ -4,8 +4,9 @@ describe "Articles" do
   describe "GET /articles" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get articles_path
-      response.status.should be(200)
+      visit articles_path
+      # page.status_code.should be(200)
+      page.should have_content("New Article")
     end
   end
 end
