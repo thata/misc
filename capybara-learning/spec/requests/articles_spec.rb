d36258a317ba12d9bf6585a63e0abf2f97b92495
thata <thata@harry.isd.ricoh.co.jp>
2011-06-14 11:12:35 +0900

    add javascript confirmation and use selenium_driver on before

--- capybara-learning/spec/requests/articles_spec.rb
+++ capybara-learning/spec/requests/articles_spec.rb
@@ -2,11 +2,19 @@ require 'spec_helper'
 
 describe "Articles" do
   describe "GET /articles" do
+    before do
+      Capybara.current_driver = :selenium
+    end
+
+    after do
+      Capybara.use_default_driver
+    end
+
     it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit articles_path
-      # page.status_code.should be(200)
       page.should have_content("New Article")
+      # javascriptが呼べている事を確認
+      evaluate_script("9 * 9").should == 81
     end
   end
 end
